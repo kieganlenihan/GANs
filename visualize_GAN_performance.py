3e674d63d610042062180198cc290466017bc5ed
kieganlenihan <kieganlenihan@gmail.com>
2021-09-07 23:27:18 -0400

    Added tertiary GAN_visualization class to show evolution of GAN model predictions

--- visualize_GAN_performance.py
+++ visualize_GAN_performance.py
@@ -5,6 +5,8 @@ import PIL.Image
 from tqdm import tqdm
 import scipy
 import moviepy.editor
+import os
+from math import floor, ceil
 
 class GAN_visualization:
     def __init__(self, path, n_img_y, n_img_x, trunc, stylegan_path):
@@ -13,7 +15,6 @@ class GAN_visualization:
         import dnnlib
         import dnnlib.tflib as tflib
         self.get_model(path)
-        self.get_tf_noise_vars()
         self.trunc = trunc
         self.n_img_x = n_img_x
         self.n_img_y = n_img_y
@@ -23,11 +24,13 @@ class GAN_visualization:
         with stream:
             _, _, Gs = pickle.load(stream, encoding = "latin1")
         self.Gs = Gs
+
+class GAN_grid_image(GAN_visualization):
+    def __init__(self):
+        self.get_tf_noise_vars()
     def get_tf_noise_vars(self):
         noise = [var for name, var in self.Gs.components.synthesis.vars.items() if name.startswith("noise")]
         self.noise_vars = noise
-
-class GAN_grid_image(GAN_visualization):
     def generate_n_latent_vectors(self, n = None):
         if n is None:
             n = self.n_img_x * self.n_img_y
@@ -72,17 +75,70 @@ class GAN_grid_image(GAN_visualization):
 
 class GAN_grid_video(GAN_visualization):
     def set_vid_params(self, ft = 20, st = 1, iz = 1, fps = 24):
-        self.frame_time = ft
+        self.duration = ft
         self.smooth_time = st
         self.zoom = iz
         self.fps = fps
         self.random_seed = np.random.randint(0, 999)
-        self.nframes = int(np.rint(self.frame_time * self.fps))
+        self.n_frames = int(np.rint(self.duration * self.fps))
         self.random_state = np.random.RandomState(self.random_seed)
     def generate_all_latents(self):
-        shape = [self.nframes, np.prod([self.n_img_x, self.n_img_y])] + self.Gs.input_shape[1:]
+        shape = [self.n_frames, np.prod([self.n_img_x, self.n_img_y])] + self.Gs.input_shape[1:]
         zs = self.random_state.randn(*shape).astype(np.float32)
         zs = scipy.ndimage.gaussian_filter(zs, [self.smooth_time * self.fps] + [0] * len(self.Gs.input_shape), mode = "wrap")
+        zs /= np.sqrt(np.mean(np.square(zs)))
+        self.zs = zs
+        return zs
+    def get_image_grid(self, imgs):
+        _, img_h, img_w, ch = imgs.shape
+        grid_w = self.n_img_x * img_w
+        grid_h = self.n_img_y * img_h
+        grid = np.zeros([grid_h, grid_w, ch], dtype = imgs.dtype)
+        for i in range(self.n_img_x):
+            for j in range(self.n_img_y):
+                x = i * img_w
+                y = j * img_h
+                grid[y : y + img_h, x : x + img_w] = imgs[i + j * self.n_img_x]
+        return grid
+    def generate_frame(self, t):
+        frame_idx = int(np.clip(np.round(t * self.fps), 0, self.n_frames - 1))
+        latent_vecs = self.zs[frame_idx]
+        fmt = dict(func = tflib.convert_images_to_uint8, nchw_to_nhwc = True)
+        imgs = self.Gs.run(latent_vecs, None, truncation_psi = self.trunc, randomize_noise = False, output_transform = fmt, minibatch_size = 16)
+        grid = self.get_image_grid(imgs)
+        return grid
+    def generate_video(self, out_path):
+        video = moviepy.editor.VideoClip(self.generate_frame, duration = self.duration)
+        video.write_videofile(out_path, fps = self.fps)
+
+class GAN_progress:
+    def __init__(self, n_img_y, n_img_x, trunc, stylegan_path):
+        sys.path.append(stylegan_path)
+        global dnnlib, tflib
+        import dnnlib
+        import dnnlib.tflib as tflib
+        self.trunc = trunc
+        self.n_img_x = n_img_x
+        self.n_img_y = n_img_y
+    def get_model(self, path):
+        stream = open(path, "rb")
+        tflib.init_tf()
+        with stream:
+            _, _, Gs = pickle.load(stream, encoding = "latin1")
+        return Gs
+    def set_vid_params(self, ft = 20, st = 1, iz = 1, fps = 24):
+        self.duration = ft
+        self.smooth_time = st
+        self.zoom = iz
+        self.fps = fps
+        self.random_seed = np.random.randint(0, 999)
+        self.n_frames = int(np.rint(self.duration * self.fps))
+        self.random_state = np.random.RandomState(self.random_seed)
+    def generate_all_latents(self, example_path):
+        Gs = self.get_model(example_path)
+        shape = [self.n_frames, np.prod([self.n_img_x, self.n_img_y])] + Gs.input_shape[1:]
+        zs = self.random_state.randn(*shape).astype(np.float32)
+        zs = scipy.ndimage.gaussian_filter(zs, [self.smooth_time * self.fps] + [0] * len(Gs.input_shape), mode = "wrap")
         zs /= np.sqrt(np.mean(np.square(zs)))
         self.zs = zs
     def get_image_grid(self, imgs):
@@ -97,12 +153,31 @@ class GAN_grid_video(GAN_visualization):
                 grid[y : y + img_h, x : x + img_w] = imgs[i + j * self.n_img_x]
         return grid
     def generate_frame(self, t):
-        frame_idx = int(np.clip(np.round(t * self.fps), 0, self.nframes - 1))
+        if not self.stop_indexing:
+            idx = ceil(t / (floor(self.n_frames / self.n_models) * 1 / self.fps))
+        if idx > self.idx:
+            self.idx = idx
+            if self.idx > self.n_models - 1:
+                self.idx = self.n_models - 1
+                idx = self.idx
+                self.stop_indexing = True
+            path = self.paths[self.idx]
+            self.Gs = self.get_model(path)          
+        frame_idx = int(np.clip(np.round(t * self.fps), 0, self.n_frames - 1))
         latent_vecs = self.zs[frame_idx]
         fmt = dict(func = tflib.convert_images_to_uint8, nchw_to_nhwc = True)
         imgs = self.Gs.run(latent_vecs, None, truncation_psi = self.trunc, randomize_noise = False, output_transform = fmt, minibatch_size = 16)
         grid = self.get_image_grid(imgs)
         return grid
-    def generate_video(self, out_path):
-        video = moviepy.editor.VideoClip(self.generate_frame, duration = self.frame_time)
+    def generate_video(self, pkl_paths, out_path):
+        self.paths = []
+        for file in os.listdir(pkl_paths):
+            file_str = str(pkl_paths + file)
+            if ".pkl" in file_str:
+                self.paths.append(file_str)
+        self.paths.sort()
+        self.idx = -1
+        self.n_models = len(self.paths)
+        self.stop_indexing = False
+        video = moviepy.editor.VideoClip(self.generate_frame, duration = self.duration)
         video.write_videofile(out_path, fps = self.fps)
